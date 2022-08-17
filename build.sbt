f06f8651f3daf54ec2081ccbe683c218cc163fe8
unknownRito <kloa_la@yahoo.com>
2022-08-17 13:00:49 -0500

    uploaded Khoa's code

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ scalaVersion := "2.11.12"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.2"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.2"
 libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.3.2"
+libraryDependencies += "org.apache.hadoop" % "hadoop-aws" % "2.7.6"
 libraryDependencies ++= Seq(
   "org.jsoup" % "jsoup" % "1.8.3"
 )
--- src/main/scala/SQLquery.scala
+++ src/main/scala/SQLquery.scala
@@ -18,31 +18,35 @@ object SQLquery{
 
         import spark.implicits._    //SQLImplicits
 
-        // val accessKey = "############"
-        // val secretAccessKey = "#########"
-        // val bucket = "################"
+        val accessKey = "######"
+        val secretAccessKey = "######"
+        val bucket = "#####"
 
-        // spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", accessKey)
-        // spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", secretAccessKey)
-        // spark.sparkContext.hadoopConfiguration.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", accessKey)
+        spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", secretAccessKey)
+        spark.sparkContext.hadoopConfiguration.set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
-        // Create DF from input parquet files
-        //val parquetDF = spark.read.parquet(s"s3a://${bucket}/optimized/2020/total_pop_2020")
-        val data00 = spark.read.parquet("total_pop_2000.parquet")
-        val data10 = spark.read.parquet("total_pop_2010.parquet")
-        val data = spark.read.parquet("total_pop_2020.parquet")
+        //Create DF from input parquet files
+        // val data00 = spark.read.parquet(s"s3a://${bucket}/optimized/2000/total_pop_2000")
+        // val data10 = spark.read.parquet(s"s3a://${bucket}/optimized/2010/total_pop_2010")
+        // val data20 = spark.read.parquet(s"s3a://${bucket}/optimized/2020/total_pop_2020")
+        val data00 = spark.read.csv("output00.csv")
+        val data10 = spark.read.csv("output10.csv")
+        val data20 = spark.read.csv("output20.csv")
 
+        
+        //val column = Seq("StateID", "RegionID", "State","TotalPop", "White", "African", "Indian", "Asian", "NativeHawaiian_PacificIslander", "OtherRace", "Hispanic_Latino")
         val column = Seq("State","TotalPop", "Hispanic_Latino", "White", "African", "Indian", "Asian", "NativeHawaiian_PacificIslander", "OtherRace", "Region")
         val df0 = data00.toDF(column:_*)
         val df1 = data10.toDF(column:_*)
-        val df = data.toDF(column:_*)
-        
+        val df2 = data20.toDF(column:_*)
+
         //Create views for the csv files to query
-        df.createOrReplaceTempView("states20")
         df0.createOrReplaceTempView("states00")
         df1.createOrReplaceTempView("states10")
+        df2.createOrReplaceTempView("states20")
 
-        //Question 4: Aggregate the total population of these different categories in 2020
+        // Question 4: Aggregate the total population of these different categories in 2020
         val q4= spark.sql("select cast(sum(white) as INT) as White, " +
                     "cast(sum(african) as INT) as African, " +
                     "cast(sum(indian) as INT) as Indian, " +
@@ -53,7 +57,7 @@ object SQLquery{
         q4.show()
         q4.write.option("header", "true").mode("overwrite").csv("input/q4/")
 
-        //Question 4: Aggregate the total population of these different categories in 2010
+        // Question 4: Aggregate the total population of these different categories in 2010
         val q4a= spark.sql("select cast(sum(white) as INT) as White, " +
                     "cast(sum(african) as INT) as African, " +
                     "cast(sum(indian) as INT) as Indian, " +
@@ -64,7 +68,7 @@ object SQLquery{
         q4a.show()
         q4a.write.option("header", "true").mode("overwrite").csv("input/q4b/")
 
-        // //Question 4: Aggregate the total population of these different categories in 2000
+        // Question 4: Aggregate the total population of these different categories in 2000
         val q4b= spark.sql("select cast(sum(white) as INT) as White, " +
                     "cast(sum(african) as INT) as African, " +
                     "cast(sum(indian) as INT) as Indian, " +
@@ -79,15 +83,15 @@ object SQLquery{
         //2000-2010
         val q8a = spark.sql("select s0.state, s0.TotalPop as `2000`, s1.TotalPop as `2010`, (s1.totalPop - s0.totalPop ) as `diffence between 2000 and 2010` " +
                             "from (states00 s0 " +
-                            "JOIN states10 s1 ON s1.state = s0.state)" +
-                            "order by `diffence between 2000 and 2010` asc").repartition(1)
+                            "JOIN states10 s1 ON s1.state = s0.state)").repartition(1)
+
+       
 
         // 2010-2020
         val q8b = spark.sql("select s1.state, s1.totalPop as `2010`, s2.totalPop as `2020`, (s2.totalPop - s1.totalPop ) as `diffence between 2010 and 2020` " +
                             "from (states10 s1 " +
-                            "JOIN states20 s2 ON s1.state = s2.state)" +
-                            "order by `diffence between 2010 and 2020` asc").repartition(1)                            
-                        
+                            "JOIN states20 s2 ON s1.state = s2.state)").repartition(1)                            
+        
         q8a.show()
         q8b.show()
         q8a.write.option("header", "true").mode("overwrite").csv("input/q8a/")
